79347429b934e56c79a4a925676c3aefcf3a07f1
x-at-01 <x-at-01@googlegroups.com>
2026-09-09 21:52:39 +0800

    feat: add remaining server stubs

--- cluster/wcluster/src/server/cluster_manager_slot_state.rs
+++ cluster/wcluster/src/server/cluster_manager_slot_state.rs
@@ -1,7 +1,21 @@
 /// libs/cluster/Server/ClusterManagerSlotState.cs:ClusterManager
-pub struct ClusterManager;
+pub struct ClusterManagerSlotState;
 
-impl ClusterManager {
-    /// libs/cluster/Server/ClusterManagerSlotState.cs:GetSlotState
-    pub fn get_slot_state(&self) {}
+impl ClusterManagerSlotState {
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryPrepareSlotForMigration
+    pub fn try_prepare_slot_for_migration(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryPrepareSlotsForMigration
+    pub fn try_prepare_slots_for_migration(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryPrepareSlotForImport
+    pub fn try_prepare_slot_for_import(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryPrepareSlotsForImport
+    pub fn try_prepare_slots_for_import(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryPrepareSlotForOwnershipChange
+    pub fn try_prepare_slot_for_ownership_change(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryPrepareSlotsForOwnershipChange
+    pub fn try_prepare_slots_for_ownership_change(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:TryResetSlotState
+    pub fn try_reset_slot_state(&self) {}
+    /// libs/cluster/Server/ClusterManagerSlotState.cs:DeleteKeysInSlots
+    pub fn delete_keys_in_slots(&self) {}
 }
