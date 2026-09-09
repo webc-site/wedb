4bd674f53591c29b0784705bb173fdcdd2baceca
x-at-01 <x-at-01@googlegroups.com>
2026-09-09 22:30:42 +0800

    feat: implement basic CLIENT, PUBSUB, and CONFIG RESP commands

--- embed/wserver/src/resp/client_commands.rs
+++ embed/wserver/src/resp/client_commands.rs
@@ -2,31 +2,116 @@ pub struct ClientCommands;
 
 impl ClientCommands {
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTLIST
-  pub fn network_clientlist() {
-    unimplemented!()
+  pub fn network_clientlist<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR CLIENT LIST is not implemented\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTINFO
-  pub fn network_clientinfo() {
-    unimplemented!()
+  pub fn network_clientinfo<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR CLIENT INFO is not implemented\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTKILL
-  pub fn network_clientkill() {
-    unimplemented!()
+  pub fn network_clientkill<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR CLIENT KILL is not implemented\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTGETNAME
-  pub fn network_clientgetname() {
-    unimplemented!()
+  pub fn network_clientgetname<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if !parse_state.is_empty() {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'client|getname' command\r\n");
+        return Ok(true);
+    }
+    // We don't have state for client name yet, return null
+    output.extend_from_slice(b"$-1\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTSETNAME
-  pub fn network_clientsetname() {
-    unimplemented!()
+  pub fn network_clientsetname<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.len() != 1 {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'client|setname' command\r\n");
+        return Ok(true);
+    }
+    // We parse the name to make sure it's valid, but don't store it since there is no state yet
+    let name = parse_state[0];
+    for &b in name {
+        if b < 0x21 || b > 0x7E {
+            output.extend_from_slice(b"-ERR Client names cannot contain spaces, newlines or special characters.\r\n");
+            return Ok(true);
+        }
+    }
+    output.extend_from_slice(b"+OK\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTSETINFO
-  pub fn network_clientsetinfo() {
-    unimplemented!()
+  pub fn network_clientsetinfo<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.len() != 2 {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'client|setinfo' command\r\n");
+        return Ok(true);
+    }
+    let option = parse_state[0];
+    if !option.eq_ignore_ascii_case(b"lib-name") && !option.eq_ignore_ascii_case(b"lib-ver") {
+        output.extend_from_slice(b"-ERR syntax error\r\n");
+        return Ok(true);
+    }
+    output.extend_from_slice(b"+OK\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/ClientCommands.cs:NetworkCLIENTUNBLOCK
-  pub fn network_clientunblock() {
-    unimplemented!()
+  pub fn network_clientunblock<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.is_empty() || parse_state.len() > 2 {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'client|unblock' command\r\n");
+        return Ok(true);
+    }
+    // Parse client id
+    let id_str = std::str::from_utf8(parse_state[0]).unwrap_or("");
+    if id_str.parse::<i64>().is_err() {
+        output.extend_from_slice(b"-ERR value is not an integer or out of range\r\n");
+        return Ok(true);
+    }
+    if parse_state.len() == 2 {
+        let option = parse_state[1];
+        if !option.eq_ignore_ascii_case(b"TIMEOUT") && !option.eq_ignore_ascii_case(b"ERROR") {
+            output.extend_from_slice(b"-ERR syntax error\r\n");
+            return Ok(true);
+        }
+    }
+    output.extend_from_slice(b":0\r\n"); // Always unblocks 0 clients currently
+    Ok(true)
   }
 }
--- embed/wserver/src/resp/pub_sub_commands.rs
+++ embed/wserver/src/resp/pub_sub_commands.rs
@@ -2,43 +2,123 @@ pub struct PubSubCommands;
 
 impl PubSubCommands {
   /// libs/server/Resp/PubSubCommands.cs:Publish
-  pub fn publish() {
-    unimplemented!()
+  pub fn publish<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR PUBLISH is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:PatternPublish
-  pub fn pattern_publish() {
-    unimplemented!()
+  pub fn pattern_publish<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR PUBLISH is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkPUBLISH
-  pub fn network_publish() {
-    unimplemented!()
+  pub fn network_publish<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.len() != 2 {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'publish' command\r\n");
+        return Ok(true);
+    }
+    output.extend_from_slice(b"-ERR PUBLISH is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkSUBSCRIBE
-  pub fn network_subscribe() {
-    unimplemented!()
+  pub fn network_subscribe<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.is_empty() {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'subscribe' command\r\n");
+        return Ok(true);
+    }
+    output.extend_from_slice(b"-ERR SUBSCRIBE is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkPSUBSCRIBE
-  pub fn network_psubscribe() {
-    unimplemented!()
+  pub fn network_psubscribe<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.is_empty() {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'psubscribe' command\r\n");
+        return Ok(true);
+    }
+    output.extend_from_slice(b"-ERR PSUBSCRIBE is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkUNSUBSCRIBE
-  pub fn network_unsubscribe() {
-    unimplemented!()
+  pub fn network_unsubscribe<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR UNSUBSCRIBE is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkPUNSUBSCRIBE
-  pub fn network_punsubscribe() {
-    unimplemented!()
+  pub fn network_punsubscribe<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR PUNSUBSCRIBE is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkPUBSUB_CHANNELS
-  pub fn network_pubsub_channels() {
-    unimplemented!()
+  pub fn network_pubsub_channels<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if parse_state.len() > 1 {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'pubsub channels' command\r\n");
+        return Ok(true);
+    }
+    output.extend_from_slice(b"-ERR PUBSUB CHANNELS is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkPUBSUB_NUMPAT
-  pub fn network_pubsub_numpat() {
-    unimplemented!()
+  pub fn network_pubsub_numpat<'a, D: wdev::Device>(
+    &mut self,
+    parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    if !parse_state.is_empty() {
+        output.extend_from_slice(b"-ERR wrong number of arguments for 'pubsub numpat' command\r\n");
+        return Ok(true);
+    }
+    output.extend_from_slice(b"-ERR PUBSUB NUMPAT is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
   /// libs/server/Resp/PubSubCommands.cs:NetworkPUBSUB_NUMSUB
-  pub fn network_pubsub_numsub() {
-    unimplemented!()
+  pub fn network_pubsub_numsub<'a, D: wdev::Device>(
+    &mut self,
+    _parse_state: &[&[u8]],
+    _store: &wkv::BatchStoreSession<'a, D>,
+    output: &mut Vec<u8>,
+  ) -> wresp::Result<bool> {
+    output.extend_from_slice(b"-ERR PUBSUB NUMSUB is disabled, enable it with --pubsub option.\r\n");
+    Ok(true)
   }
 }
