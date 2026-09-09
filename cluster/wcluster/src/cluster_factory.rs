00dd12a5418f58134f986a8afb5b599ca55ad826
x-at-01 <x-at-01@googlegroups.com>
2026-09-09 21:51:48 +0800

    feat: add ClusterFactory and SessionParseStateExtensions

---
+++ cluster/wcluster/src/cluster_factory.rs
@@ -0,0 +1,7 @@
+/// libs/cluster/ClusterFactory.cs:ClusterFactory
+pub struct ClusterFactory;
+
+impl ClusterFactory {
+    /// libs/cluster/ClusterFactory.cs:CreateClusterProvider
+    pub fn create_cluster_provider(&self) {}
+}
---
+++ cluster/wcluster/src/session_parse_state_extensions.rs
@@ -0,0 +1,7 @@
+/// libs/cluster/SessionParseStateExtensions.cs:SessionParseStateExtensions
+pub struct SessionParseStateExtensions;
+
+impl SessionParseStateExtensions {
+    /// libs/cluster/SessionParseStateExtensions.cs:CheckACLPermissions
+    pub fn check_acl_permissions(&self) {}
+}
