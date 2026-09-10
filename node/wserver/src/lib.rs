64ff2e29352fd9660d5b7dd3b5e51eb62c4db9ec
x-at-01 <x-at-01@googlegroups.com>
2026-09-10 10:39:27 +0800

    .

--- node/wserver/src/lib.rs
+++ node/wserver/src/lib.rs
@@ -4,9 +4,6 @@
 //! （如 `LuaRunner_Functions` 对应 LuaRunner.Functions.cs 分片），
 //! 故 crate 级放宽 non_camel_case_types/non_snake_case；其余 lint 不放宽。
 
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 pub mod arg_slice;
 pub mod input_header;
 pub mod inputs;
